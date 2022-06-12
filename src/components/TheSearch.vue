f1436e54ac6d95bc83b076784c53612728152157
cx33 <lne128gene980@163.com>
2022-06-12 16:39:53 +0800

    update: loading

--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -7,13 +7,17 @@ const { refresh, blockChain, errorMsg, loading } = useBlockChain(blockHash)
 <template>
   <div flex="c col">
     <input v-model="blockHash" w="80%" p-2 my-2 rounded border="~ gray-400/50" type="text" placeholder="input a block hash and keydown enter to search" @keydown.enter="refresh">
-    <TheLoading :loading="loading" />
-    <div v-if="blockChain">
-      {{ blockChain }}
+    <div v-if="loading">
+      <TheLoading :loading="loading" />
     </div>
-    <div v-if="!loading && !blockChain">
-      {{ errorMsg }}
-    </div>
+    <template v-else>
+      <div v-if="blockChain">
+        {{ blockChain }}
+      </div>
+      <div v-else>
+        {{ errorMsg }}
+      </div>
+    </template>
   </div>
 </template>
 
